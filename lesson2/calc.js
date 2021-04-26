72b453bf1e6cd45c188e5a7a22cf34f0131e60e4
shimri <shimridevsolutions@gmail.com>
2021-04-26 12:54:04 +0300

    add js file

---
+++ lesson2/calc.js
@@ -0,0 +1,15 @@
+let sum = 0;
+
+let screen = document.querySelector(".screen");
+// screen.innerText = "This is my screen";
+
+let btns = document.querySelectorAll(".btn-calc-num");
+
+btns.forEach((btn, index) => {
+  btn.addEventListener("click", (e) => {
+    sum += +e.target.textContent;
+    console.log(`total sum ${sum}`);
+
+    screen.innerHTML += `<button class="btn-calc-num" id="num7">${e.target.textContent}</button>`;
+  });
+});
